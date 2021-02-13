7e869619db830c881bbee6b6270ab19c8b47a301
Eugene Zamriy <eugene@zamriy.info>
2021-02-13 20:46:32 +0300

    0.1.1 version
      - improved error handling for curl calls
      - implemented colorful output
      - script output reworked

--- almalinux-deploy.sh
+++ almalinux-deploy.sh
@@ -7,18 +7,34 @@
 
 set -euo pipefail
 
-VERSION='0.1.0'
-OS_RELEASE_PATH='/etc/os-release'
 BASE_TMP_DIR='/root'
+OS_RELEASE_PATH='/etc/os-release'
+VERSION='0.1.1'
 
-
-# Prints an error message to stderr.
-show_error() {
+# Reports a completed step using a green color.
+#
+# $1 - Message to print.
+report_step_done() {
     local -r message="${1}"
-    echo "Error: ${message}" >&2
+    printf '\033[0;32m%-70sOK\033[0m\n' "${message}"
 }
 
+# Reports a failed step using a red color.
+#
+# $1 - Message to print.
+# $2 - Additional information to show (optional).
+report_step_error() {
+    local -r message="${1}"
+    local -r trace="${2:-}"
+    printf '\033[0;31m%-70sERROR\033[0m\n' "${message}" 1>&2
+    if [[ -n "${trace}" ]]; then
+        echo "${trace}" | while read -r line; do
+            printf '    %s\n' "${line}" 1>&2
+        done
+    fi
+}
 
+# Prints program usage information.
 show_usage() {
     echo -e 'Migrates an EL system to AlmaLinux\n'
     echo -e 'Usage: almalinux-deploy.sh [OPTION]...\n'
@@ -29,9 +45,11 @@ show_usage() {
 # Terminates the program if it is not run with root privileges
 assert_run_as_root() {
     if [[ $(id -u) -ne 0 ]]; then
-        echo 'Error: the migration tool must be run as root' >&2
+        report_step_error 'Check root privileges' \
+            'Migration tool must be run as root'
         exit 2
     fi
+    report_step_done 'Check root privileges'
 }
 
 # Prints a system architecture.
@@ -64,19 +82,19 @@ assert_supported_system() {
     local -r os_version="${2}"
     local -r arch="${3}"
     if [[ ${arch} != 'x86_64' ]]; then
-        show_error "${arch} architecture is not supported"
+        report_step_error "Check ${arch} architecture is supported"
         exit 1
     fi
     if [[ ${os_version} -ne 8 ]]; then
-        show_error "EL${os_version} is not supported"
+        report_step_error "Check EL${os_version} is supported"
         exit 1
     fi
     if [[ ${os_id} != 'centos' ]]; then
-        show_error "migration from ${os_id} is not supported"
+        report_step_error "Check ${os_id} operating system is supported"
         exit 1
     fi
+    report_step_done "Check ${os_id}-${os_version}.${arch} is supported"
 }
-
 
 # Returns a latest almalinux-release RPM package download URL.
 #
@@ -97,9 +115,16 @@ install_rpm_pubkey() {
     local -r tmp_dir="${1}"
     local -r pubkey_url="${ALMA_PUBKEY_URL:-https://repo.almalinux.org/almalinux/RPM-GPG-KEY-AlmaLinux}"
     local -r pubkey_path="${tmp_dir}/RPM-GPG-KEY-AlmaLinux"
-    echo -e "Installing AlmaLinux public key: ${pubkey_url}"
-    curl -s -o "${pubkey_path}" "${pubkey_url}"
+    local -r step='Download RPM-GPG-KEY-AlmaLinux'
+    local output
+    if ! output=$(curl -f -s -S -o "${pubkey_path}" "${pubkey_url}" 2>&1); then
+        report_step_error "${step}" "${output}"
+        exit 1
+    else
+        report_step_done "${step}"
+    fi
     rpm --import "${pubkey_path}"
+    report_step_done 'Import RPM-GPG-KEY-AlmaLinux to RPM DB'
     rm -f "${pubkey_path}"
 }
 
@@ -113,7 +138,11 @@ download_release_file() {
     local -r release_url="${1}"
     local -r tmp_dir="${2}"
     local -r release_path="${tmp_dir}/almalinux-release-latest.rpm"
-    curl -s -o "${release_path}" "${release_url}"
+    local output
+    if ! output=$(curl -f -s -S -o "${release_path}" "${release_url}" 2>&1); then
+        report_step_error 'Download almalinux-release package' "${output}"
+        exit 1
+    fi
     echo "${release_path}"
 }
 
@@ -123,18 +152,23 @@ download_release_file() {
 assert_valid_package() {
     local -r pkg_path="${1}"
     local output
-    if ! output=$(rpm -K "${pkg_path}"); then
-        show_error "${output}"
+    if ! output=$(rpm -K "${pkg_path}" 2>&1); then
+        report_step_error "Verify $(basename "${pkg_path}") package" \
+            "${output}"
         exit 1
     fi
 }
 
-
+# Recursively removes a given directory.
+#
+# $1 - Directory path.
 cleanup_tmp_dir() {
     rm -fr "${1:?}"
 }
 
-
+# Converts a CentOS system to AlmaLinux
+#
+# $1 - almalinux-release RPM package path.
 migrate_from_centos() {
     local -r release_path="${1}"
     local to_remove=''
@@ -145,25 +179,29 @@ migrate_from_centos() {
         fi
     done
     if [[ -n "${to_remove}" ]]; then
-        echo "Removing CentOS packages: ${to_remove}"
         # shellcheck disable=SC2086
         rpm -e --nodeps ${to_remove}
+        for pkg_name in ${to_remove}; do
+            report_step_done "Remove ${pkg_name} package"
+        done
     fi
-    echo "Installing almalinux-release"
     rpm -Uvh "${release_path}"
+    report_step_done 'Install almalinux-release'
 }
 
-
+# Executes the 'dnf distro-sync -y' command.
+#
+# Returns the dnf command exit code.
 distro_sync() {
+    local -r step='Run dnf distro-sync -y'
     local ret_code=0
-    echo 'Reinstalling packages from AlmaLinux repositories: dnf distro-sync -y'
     dnf distro-sync -y || {
         ret_code=${?}
-        show_error "dnf distro-sync failed with ${ret_code} exit code"
+        report_step_error "${step}. Exit code: ${ret_code}"
     }
+    report_step_done "${step}"
     return ${ret_code}
 }
-
 
 main() {
     local arch
@@ -184,27 +222,25 @@ main() {
     trap "cleanup_tmp_dir ${tmp_dir}" EXIT
     install_rpm_pubkey "${tmp_dir}"
 
-    echo "Downloading almalinux-release package: ${release_url}"
     release_path=$(download_release_file "${release_url}" "${tmp_dir}")
+    report_step_done 'Download almalinux-release package'
 
-    echo "Verifying almalinux-release package: ${release_path}"
     assert_valid_package "${release_path}"
-    
+    report_step_done 'Verify almalinux-release package'
+
     case "${os_id}" in
     centos)
         migrate_from_centos "${release_path}"
         ;;
     *)
-        show_error "${os_id} is not supported"
+        report_step_error "Migrate ${os_id}: not supported"
         exit 1
         ;;
     esac
 
     distro_sync || exit ${?}
-
-    echo 'Migration to AlmaLinux is completed, please reboot the system'
+    printf '\n\033[0;32mMigration to AlmaLinux is completed, please reboot the system\033[0m\n'
 }
-
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
     for opt in "$@"; do
--- test_almalinux-deploy.bats
+++ test_almalinux-deploy.bats
@@ -13,16 +13,6 @@ teardown() {
     fi
 }
 
-# assert_get_system_arch_fails() {
-#     local -r arch="${1}"
-#     eval "function uname() { echo ${arch}; }"
-#     export -f uname
-#     run get_system_arch
-#     [[ ${status} -ne 0 ]]
-#     [[ ${output} =~ 'Error' ]]
-# }
-
-
 @test 'get_system_arch returns x86_64 architecture' {
     function uname() { echo 'x86_64'; }
     export -f uname
@@ -38,22 +28,6 @@ teardown() {
     [[ ${status} -eq 0 ]]
     [[ ${output} == 'aarch64' ]]
 }   
-
-
-# @test 'get_system_arch fails on i686 architecture' {
-#     assert_get_system_arch_fails i686
-# }
-
-
-# @test 'get_system_arch fails on armv7l architecture' {
-#     assert_get_system_arch_fails armv7l
-# }
-
-
-# @test 'get_system_arch fails on aarch64 architecture' {
-#     assert_get_system_arch_fails aarch64
-# }
-
 
 @test 'assert_run_as_root passes for root' {
     function id() { echo 0; }
@@ -102,7 +76,7 @@ teardown() {
     export ALMA_RELEASE_URL='https://example.com/almalinux-release-latest-8.aarch64.rpm'
     run get_release_file_url '8' 'aarch64'
     [[ ${status} -eq 0 ]]
-    [[ ${output} == ${ALMA_RELEASE_URL} ]]
+    [[ ${output} == "${ALMA_RELEASE_URL}" ]]
 }
 
 @test 'assert_supported_system passes on CentOS-8 x86_64' {
@@ -114,7 +88,7 @@ teardown() {
     for arch in 'i686' 'aarch64' 'armv7l'; do
         run assert_supported_system 'centos' '8' "${arch}"
         [[ ${status} -ne 0 ]]
-        [[ ${output} == *"architecture is not supported" ]]
+        [[ ${output} =~ 'ERROR' ]]
     done
 }
 
@@ -122,7 +96,7 @@ teardown() {
     for os_version in 6 7 9; do
         run assert_supported_system 'centos' "${os_version}" 'x86_64'
         [[ ${status} -ne 0 ]]
-        [[ ${output} == *"EL${os_version} is not supported" ]]
+        [[ ${output} =~ 'ERROR' ]]
     done
 }
 
@@ -130,6 +104,6 @@ teardown() {
     for os_id in 'fedora' 'ol' 'rhel'; do
         run assert_supported_system "${os_id}" '8' 'x86_64'
         [[ ${status} -ne 0 ]]
-        [[ ${output} == *"migration from ${os_id} is not supported" ]]
+        [[ ${output} =~ 'ERROR' ]]
     done
 }
