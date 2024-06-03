6c44b5f0f149223a82df8ca7ef510f4ec82a4f17
Yuriy Kohut <yura.kohut@gmail.com>
2024-06-03 13:47:21 +0300

    Add openssl-fips-provider to the list of packages to be removed. (#219)

--- almalinux-deploy.sh
+++ almalinux-deploy.sh
@@ -45,7 +45,7 @@ REMOVE_PKGS=("centos-linux-release" "centos-gpg-keys" "centos-linux-repos" \
                 "rocky-release" "rocky-gpg-keys" "rocky-repos" \
                 "rocky-obsolete-packages" "libblockdev-btrfs" \
                 "vzlinux-release" "vzlinux-gpg-keys" "vzlinux-repos" "vzlinux-obsolete-packages" \
-                "evolution-data-server-ui" "epel-next-release")
+                "evolution-data-server-ui" "epel-next-release" "openssl-fips-provider")
 REDHAT_DNF_PLUGINS=("product-id" "subscription-manager" "upload-profile")
 REDHAT_REPO_FILES=("/etc/yum.repos.d/redhat.repo" "/etc/yum.repos.d/ubi.repo")
 REDHAT_RHSM_RPMS=("subscription-manager" "subscription-manager-cockpit" "cockpit" "rhc")
