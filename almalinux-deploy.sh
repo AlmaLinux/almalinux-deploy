2ae5007c71d2611169831a48d91738bc9e37d872
nlaphine <156731130+nlaphine@users.noreply.github.com>
2026-08-29 09:56:19 +1000

    Update almalinux-deploy.sh
    Co-authored-by: Yuriy Kohut <yura.kohut@gmail.com>

--- almalinux-deploy.sh
+++ almalinux-deploy.sh
@@ -1511,7 +1511,6 @@ main() {
     local panel_type
     local panel_version
     assert_run_as_root
-    setup_preserve_rhsm_hook
     arch="$(get_system_arch)"
     os_type="$(get_os_release_var 'ID')"
     os_version="$(get_os_version "${os_type}")"
